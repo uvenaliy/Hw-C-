16b36991309f138ae1b181385e046e01fe1bce12
Uvenalii <uvenal93@mail.ru>
2023-01-06 01:08:41 +0500

    add Sem 6 Task (39-44)

---
+++ lessons/Sem6Task41/Program.cs
@@ -0,0 +1,31 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int number = ReadData("Введите количество чисел: ");
+int countPositivNum = CountPositivNum(number);
+PrintData(" Количество чисел больше нуля: " + countPositivNum);
+
+
+// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Метод выводит данные
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+// Метод подсчета чисел больше нуля
+int CountPositivNum(int num)
+{
+    int res = 0;
+    while (num > 0)
+    {
+        if (ReadData(" Введите число и нажмите Enter: ") > 0) res++;
+        num = num - 1;
+    }
+    return res;
+}
---
+++ lessons/Sem6Task43/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+double b1 = ReadData("Введите b1: ");
+double k1 = ReadData("Введите k1: ");
+double b2 = ReadData("Введите b2: ");
+double k2 = ReadData("Введите k2: ");
+double res = IntersectionPoint(b1, k1, b2, k2);
+PrintData("Пересечение в точке: " + res);
+
+
+// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Метод выводит результат пользователю
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+double IntersectionPoint(double b1, double b2, double k1, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    return x;
+    return y;
+}
+
