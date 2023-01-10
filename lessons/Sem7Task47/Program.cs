a1e1d05110667d2d59e1e03d971a07774313c83c
Yuvenaliy <119494937+uvenaliy@users.noreply.github.com>
2023-01-10 21:57:51 +0500

    Update Program.cs

--- lessons/Sem7Task47/Program.cs
+++ lessons/Sem7Task47/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами
+
+int row = ReadData("Введите количество строк ");
+int column = ReadData("Введите количество столбцов ");
+double[,] arr2D = Fill2DArray(row, column, 10, 50);
+Print2DArrayColor(arr2D);
+
+
+// Метод считывает данные пользователя
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Универсальный метод генерации и заполнения двумерного массива
+double[,] Fill2DArray(int countRow, int countColumn, double topBorder, double downBorder)
+{
+    System.Random rand = new System.Random();
+    double[,] array2D = new double[countRow, countColumn];
+
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array2D[i, j] = new Random().Next(10, 50) / 10.0;
+        }
+    }
+    return array2D;
+}
+
+// Метод цветного вывода двумерного массива
+void Print2DArrayColor(double[,] matrix)
+
+ {ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.ForegroundColor = col[new System.Random().Next(0, 16)];
+            Console.Write(matrix[i, j] + "     ".Substring(matrix[i, j].ToString().Length));
+            Console.ResetColor();
+        }
+        Console.WriteLine();
+}
+ }
