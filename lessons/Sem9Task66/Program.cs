187668cf4e23f79c80b7c2f70139ea2063fb8abc
Uvenalii <uvenal93@mail.ru>
2023-01-15 20:10:54 +0500

    add Sem 9

---
+++ lessons/Sem9Task66/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N
+
+//Чтение данных с консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Печать результата
+void PrintData(int res)
+{
+    Console.WriteLine(res);
+}
+
+// Метод нахождения суммы
+int SumMNRec(int m, int n)
+{
+    if (m >= n) return n;
+    int outLine = m + SumMNRec(m + 1, n);
+    return outLine;
+}
+
+int m = ReadData("Введите число M: ");
+int n = ReadData("Введите число N: ");
+int result = 0;
+if (m < n) result = SumMNRec(m, n);
+else result = SumMNRec(n, m);
+PrintData(result);
---
+++ lessons/Sem9Task68/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n
+
+//Чтение данных с консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Печать результата
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+// Функция Аккермана
+int AckermannFunc(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (m > 0 && n == 0)
+    {
+        return AckermannFunc(m - 1, 1);
+    }
+    else
+    {
+        return AckermannFunc(m - 1, AckermannFunc(m, n - 1));
+    }
+}
+
+
+int m = ReadData("Введите неотрицательное число M: ");
+int n = ReadData("Введите неотрицательное число N: ");
+if (m >= 0 && n >= 0)
+{
+    PrintResult($"Функция Аккермана A({m},{n}) = {AckermannFunc(m, n)}");
+}
+else 
+{
+    PrintResult("Необходимо использовать неотрицательные числа");
+}
